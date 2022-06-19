3d0bbe3d71882ad2167539f4b82f6501b04cd3ef
83Rafa <elfaramail@gmail.com>
2022-06-19 17:56:06 -0300

    created string array of test class

--- src/main/kotlin/one/digitalinnovation/collections/TestIntArrayOf.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestIntArrayOf.kt
@@ -1,2 +1,15 @@
 package one.digitalinnovation.collections
 
+fun main() {     //inicialização sem a necessidade de definir o tamanho do array
+    val values = intArrayOf(2, 3, 4, 1, 10, 7) // array inicializado com valores e referência "values"
+
+    values.forEach {
+        println(it)
+    }
+
+    println(" ")
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+}
